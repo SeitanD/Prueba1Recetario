7d458742fcd87e7e577e76398e514385769b4b92
Sebastian Diaz <sn.diaz@duocuc.cl>
2024-04-13 05:02:18 -0400

    Agregado el Modal en el Index

--- script.js
+++ script.js
@@ -14,3 +14,29 @@ function seleccionar(){
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
+// Función para mostrar el modal
+function mostrarModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+}
+
+// Función para cerrar el modal
+function cerrarModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+
+// Función para seleccionar
+function seleccionar() {
+    mostrarModal();
+}
+
+// Función para mostrar u ocultar el menú responsive
+function mostrarOcultarMenu() {
+    var nav = document.getElementById("nav");
+    if (nav.style.display === "none" || nav.style.display === "") {
+        nav.style.display = "block";
+    } else {
+        nav.style.display = "none";
+    }
+}
